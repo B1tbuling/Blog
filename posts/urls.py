db104f409cfc5647e8bb6d71c76411e29022420e
slr <B1tbuling>
2022-09-20 00:29:35 +0300

    refactor

--- posts/urls.py
+++ posts/urls.py
@@ -3,18 +3,16 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', get_posts_list, name='get_posts_list_url'),
     path('<int:pk>/', get_post_page, name='get_post_page_url'),
-    path('create/', get_form_create_post, name='get_form_create_post'),
-    path('add/', create_post, name='add_form_create_post'),
-    path('update/<int:pk>', get_form_update_post, name='get_form_update_post'),
-    path('change/<int:pk>', update_post, name='add_form_update_post'),
+    path('create/', create_post, name='create_post_url'),
+    path('update/<int:pk>', update_post, name='update_post_url'),
+    path('delete/<int:pk>', delete_post, name='delete_post_url'),
+
     path('tags/', get_tags_list, name='get_tags_list_url'),
-    path('tags/create', get_form_create_tag, name='get_form_create_tag_url'),
-    path('tags/add', add_form_create_tag, name='add_form_create_tag_url'),
-    path('tags/update/<str:slug>', get_form_update_tag, name='get_form_update_tag_url'),
-    path('tags/change/<str:slug>', change_form_update_tag, name='change_form_update_tag_url'),
+    path('tags/create', create_tag, name='create_tag_url'),
+    path('tags/update/<str:slug>', update_tag, name='update_tag_url'),
+    path('delete/<str:slug>', delete_tag, name='delete_tag_url'),
     path('tags/<str:slug>', get_tag_page, name='get_tag_page_url')
 
 ]
--- posts/views.py
+++ posts/views.py
@@ -7,92 +7,90 @@ from django.http import HttpResponse
 def get_post_page(request, pk):
     post = Posts.objects.get(id=pk)
     tags = post.tags.all()
-    return render(request, 'post_page.html', context={'post': post, 'tags': tags})
+    return render(request, 'posts/post_page.html', context={'post': post, 'tags': tags})
 
 
 def get_posts_list(request):
     posts = Posts.objects.all()
-    return render(request, 'posts_list.html', context={'posts': posts})
-
-
-def get_form_create_post(request):
-    form = PostModelForm()
-    tags = Tag.objects.all()
-    return render(request, 'post_create.html', context={'form': form, 'tags': tags})
+    return render(request, 'posts/posts_list.html', context={'posts': posts})
 
 
 def create_post(request):
-    # form_data = PostForm(request.POST)
+    if request.method == 'GET':
+        form = PostModelForm()
+        tags = Tag.objects.all()
+        return render(request, 'posts/post_create.html', context={'form': form, 'tags': tags})
 
-    form_post = PostModelForm(request.POST)
-    print(request.POST)
-    if form_post.is_valid():
-        post = form_post.save()
-        # data = dict(request.POST)
-        # title = data['title'][0]
-        # text = data['text'][0]
-        # post = Posts(title=title, text=text)
-        # post.save()
-        # post = Posts.objects.create(title=title, text=text)
-        return redirect(reverse('get_post_page_url', kwargs={'pk': post.id}))
-    else:
-        return redirect('create_post_url')
+    if request.method == 'POST':
+        form_post = PostModelForm(request.POST)
+        print(request.POST)
+        if form_post.is_valid():
+            post = form_post.save()
+            return redirect(reverse('get_post_page_url', kwargs={'pk': post.id}))
+        else:
+            return redirect('create_post_url')
 
-
-def get_form_update_post(request, pk):
-    post = Posts.objects.get(id=pk)
-    # form = PostModelForm({'title': post.title, 'text': post.text})
-    form = PostModelForm(instance=post)
-    return render(request, 'post_update.html', context={'form': form, 'post_id': pk})
+    return HttpResponse(status=405)
 
 
 def update_post(request, pk):
-    post = Posts.objects.get(id=pk)
-    form_data = PostModelForm(request.POST, instance=post)
-    if form_data.is_valid():
-        form_data.save()
-        # data = dict(request.POST)
-        # post = Posts.objects.get(id=pk)
-        # post.title = data['title'][0]
-        # post.text = data['text'][0]
-        # post.save()
-        return redirect(reverse('get_post_page_url', kwargs={'pk': post.id}))
-    return redirect(reverse('get_form_update_post', kwargs={'pk': pk}))
+    if request.method == 'GET':
+        post = Posts.objects.get(id=pk)
+        form = PostModelForm(instance=post)
+        return render(request, 'posts/post_update.html', context={'form': form, 'post_id': pk})
+
+    if request.method == 'POST':
+        post = Posts.objects.get(id=pk)
+        form_data = PostModelForm(request.POST, instance=post)
+        if form_data.is_valid():
+            form_data.save()
+            return redirect(reverse('get_post_page_url', kwargs={'pk': post.id}))
+        return redirect(reverse('update_post_url', kwargs={'pk': pk}))
+
+    return HttpResponse(status=405)
+
+
+def delete_post(request, pk): ...
 
 
 def get_tag_page(request, slug):
     tag = Tag.objects.get(tag__iexact=slug)
     posts = tag.posts.all()
     print(posts)
-    return render(request, 'tag_page.html', context={'tag': tag, 'posts': posts})
+    return render(request, 'tags/tag_page.html', context={'tag': tag, 'posts': posts})
 
 
 def get_tags_list(request):
     tags = Tag.objects.all()
-    return render(request, 'tags_list.html', context={'tags': tags})
+    return render(request, 'tags/tags_list.html', context={'tags': tags})
 
 
-def get_form_create_tag(request):
-    tag_form = TagModelForm()
-    return render(request, 'tag_create.html', context={'tag_form': tag_form})
+def create_tag(request):
+    if request.method == 'GET':
+        tag_form = TagModelForm()
+        return render(request, 'tags/tag_create.html', context={'tag_form': tag_form})
+
+    if request.method == 'POST':
+        form_data = TagModelForm(request.POST)
+        if form_data.is_valid():
+            new_tag = form_data.save()
+            return redirect(reverse('get_tag_page_url', kwargs={'slug': new_tag.tag}))
+
+    return HttpResponse(status=405)
 
 
-def add_form_create_tag(request):
-    form_data = TagModelForm(request.POST)
-    if form_data.is_valid():
-        new_tag = form_data.save()
-        return redirect(reverse('get_tag_page_url', kwargs={'slug': new_tag.tag}))
+def update_tag(request, slug):
+    if request.method == 'GET':
+        tag = Tag.objects.get(tag__iexact=slug)
+        tag_form = TagModelForm(instance=tag)
+        return render(request, 'tags/tag_update.html', context={'tag_form': tag_form, 'slug': slug})
+
+    if request.method == 'POST':
+        tag = Tag.objects.get(tag=slug)
+        form_data = TagModelForm(request.POST, instance=tag)
+        if form_data.is_valid():
+            new_tag = form_data.save()
+            return redirect(reverse('get_tag_page_url', kwargs={'slug': new_tag.tag}))
 
 
-def get_form_update_tag(request, slug):
-    tag = Tag.objects.get(tag__iexact=slug)
-    tag_form = TagModelForm(instance=tag)
-    return render(request, 'tag_update.html', context={'tag_form': tag_form, 'slug': slug})
-
-
-def change_form_update_tag(request, slug):
-    tag = Tag.objects.get(tag=slug)
-    form_data = TagModelForm(request.POST, instance=tag)
-    if form_data.is_valid():
-        new_tag = form_data.save()
-        return redirect(reverse('get_tag_page_url', kwargs={'slug': new_tag.tag}))
+def delete_tag(request, slug): ...
